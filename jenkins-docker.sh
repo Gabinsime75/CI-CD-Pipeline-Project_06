912ec012c49b20a45021b2f0ae1081e8c07c78a9
Gabin Sime <gabin75@yahoo.de>
2023-08-26 01:37:57 -0700

    added all the files

---
+++ jenkins-docker.sh
@@ -0,0 +1,16 @@
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-11-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+sudo yum update -y
+sudo dnf install docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker jenkins
